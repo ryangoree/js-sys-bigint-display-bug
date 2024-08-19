3dcd867982f2f527d72926ee63ecb11664388829
Ryan Goree <goree.ryan@gmail.com>
2024-08-19 15:38:54 -0500

    Add comment showing error

--- src/lib.rs
+++ src/lib.rs
@@ -5,4 +5,9 @@ use wasm_bindgen_test::*;
 fn pass() {
     let formatted_negative_bigint = format!("{}", BigInt::from(-1));
     assert_eq!(formatted_negative_bigint, "-1");
+    // error output:
+    //     panicked at src/lib.rs:7:5:
+    //     assertion `left == right` failed
+    //       left: "--1"
+    //      right: "-1"
 }
